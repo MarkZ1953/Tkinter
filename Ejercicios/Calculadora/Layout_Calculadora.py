f7032f80465a42bc6ff128c8dbd05027f678635c
Felipe Castro <castrolopez.davidfelipe@gmail.com>
2023-03-27 22:00:04 -0500

    .

--- Ejercicios/Calculadora/Layout_Calculadora.py
+++ Ejercicios/Calculadora/Layout_Calculadora.py
@@ -52,51 +52,39 @@ class Layout_Calculadora:
         botones = {
             "C": (0, 0),
             "/": (0, 3),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
-            "C": (0, 0),
+            "7": (1, 0),
+            "8": (1, 1),
+            "9": (1, 2),
+            "*": (1, 3),
+            "4": (2, 0),
+            "5": (2, 1),
+            "6": (2, 2),
+            "-": (2, 3),
+            "1": (3, 0),
+            "2": (3, 1),
+            "3": (3, 2),
+            "+": (3, 3),
+            "0": (4, 0),
+            ".": (4, 2),
+            "=": (4, 3)
         }
 
         for texto_boton, posicion in botones.items():
-            boton = ctk.CTkButton(self.frame2, text=texto_boton)
+            # Creamos los botones
+            boton = ctk.CTkButton(self.frame2, text=texto_boton, cursor="hand2", corner_radius=0, border_width=1,
+                                  border_color="white", fg_color="white", text_color="grey", font=("Calibri", 25),
+                                  hover_color="#eee", command=lambda k=texto_boton: self.boton_pulsado(k))
 
-        for i in range(2):
-            bton = ctk.CTkButton(self.frame2, text=nom_botones[i], cursor="hand2", corner_radius=0, border_width=1,
-                                 border_color="white", fg_color="#eee", text_color="grey", font=("Calibri", 25),
-                                 hover_color="#eee", command=lambda k=nom_botones[i]: self.boton_pulsado(k))
-            if i == 0:
-                bton.grid(row=0, columnspan=3, sticky="nswe", padx=1, pady=1)
+            # Se hace eso para aplicar los colores respectivos a cada boton
+            if texto_boton in ["C", "/", "*", "-", "+", "."]:
+                boton.configure(fg_color="#eee")
+            elif texto_boton == "=":
+                boton.configure(fg_color="#084474", text_color="white", hover_color="#1c5484")
+
+            # Se separan los grid ya que el boton "C" y "0" son mas grandes que los demas
+            if texto_boton == "C":
+                boton.grid(row=0, columnspan=3, sticky="news", padx=1, pady=1)
+            elif texto_boton == "0":
+                boton.grid(row=4, columnspan=2, sticky="news", padx=1, pady=1)
             else:
-                bton.grid(row=0, column=3, sticky="nswe", padx=1, pady=1)
-
-        for i in range(3):
-            for j in range(4):
-                bton = ctk.CTkButton(self.frame2, text=nom_botones[c], cursor="hand2", corner_radius=0, border_width=1,
-                                     border_color="white", fg_color="white", text_color="grey", font=("Calibri", 25),
-                                     hover_color="#eee", command=lambda k=nom_botones[c]: self.boton_pulsado(k))
-                bton.grid(row=i + 1, column=j, sticky="nswe", padx=1, pady=1)
-                if j == 3:
-                    bton.configure(fg_color="#eee")
-                c += 1
-        c = 14
-        for i in range(3):
-            bton = ctk.CTkButton(self.frame2, text=nom_botones[c], cursor="hand2", corner_radius=0, border_width=1,
-                                 border_color="white", fg_color="white", text_color="grey", font=("Calibri", 25),
-                                 hover_color="#eee", command=lambda k=nom_botones[c]: self.boton_pulsado(k))
-            if i == 0:
-                bton.grid(row=4, columnspan=2, sticky="nswe", padx=1, pady=1)
-            else:
-                bton.grid(row=4, column=i + 1, sticky="nswe", padx=1, pady=1)
-            c += 1
-
-            if c == 17:
-                bton.configure(fg_color="#084474", text_color="white", hover_color="#1c5484")
-            elif c == 16:
-                bton.configure(fg_color="#eee")
+                boton.grid(row=posicion[0], column=posicion[1], sticky="news", padx=1, pady=1)
